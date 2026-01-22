f4de5775e75aec1d5d12adc33ca35e5f7f4a89a0
Antony Seas <83423392+ToniSeas@users.noreply.github.com>
2026-01-21 20:39:34 -0600

    Add TsvrcOverlayContainer class for overlay management and player tracking

---
+++ Scripts/UI/TsvrcOverlayContainer.cs
@@ -0,0 +1,58 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+
+namespace Tsvrc.UI
+{
+    public class TsvrcOverlayContainer : UdonSharpBehaviour
+    {
+        public Canvas canvas;
+        public Material overlayMaterial;
+
+        #region Unity Callbacks
+
+        protected void Start()
+        {
+            if (overlayMaterial != null)
+            {
+                // Set material on all MeshRenderers in child objects
+                MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>(true);
+                foreach (MeshRenderer renderer in renderers)
+                {
+                    renderer.material = overlayMaterial;
+                }
+
+                // Set material on all UI Images in child objects
+                Image[] images = GetComponentsInChildren<Image>(true);
+                foreach (Image image in images)
+                {
+                    image.material = overlayMaterial;
+                }
+
+                // Set material on all UI RawImages in child objects
+                RawImage[] rawImages = GetComponentsInChildren<RawImage>(true);
+                foreach (RawImage rawImage in rawImages)
+                {
+                    rawImage.material = overlayMaterial;
+                }
+            }
+        }
+
+        protected void Update()
+        {
+            VRCPlayerApi localPlayer = Networking.LocalPlayer;
+            if (localPlayer == null) return;
+
+            // Get player head position and rotation
+            Vector3 headPosition = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
+            Quaternion headRotation = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation;
+
+            gameObject.transform.position = headPosition + headRotation * Vector3.forward;
+            gameObject.transform.rotation = headRotation;
+        }
+
+        #endregion
+    }
+}
