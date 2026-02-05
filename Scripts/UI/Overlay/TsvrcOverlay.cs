99dab98fccd1fd242bbeab1d6cd7e280e9408bbd
Antony Seas <83423392+ToniSeas@users.noreply.github.com>
2026-02-04 21:49:47 -0600

    Add better initial overlay logic

---
+++ Scripts/UI/Overlay/TsvrcOverlay.cs
@@ -0,0 +1,47 @@
+﻿using Tsvrc.Core;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Tsvrc.UI.Overlay
+{
+    public class TsvrcOverlay : TsvrcBehaviour
+    {
+        public Transform PlayerHeadPosition;
+        public GameObject Container;
+        public Transform DesktopDisplay;
+
+
+        private void Update()
+        {
+            VRCPlayerApi localPlayer = Networking.LocalPlayer;
+            if (localPlayer == null) return;
+
+            Vector3 headPosition = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
+            Quaternion headRotation = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation;
+
+            Container.transform.position = headPosition - headRotation * PlayerHeadPosition.localPosition;
+            Container.transform.rotation = headRotation;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (Container != null)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireCube(Container.transform.position, Container.transform.lossyScale);
+            }
+
+            if (PlayerHeadPosition != null)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(PlayerHeadPosition.position, 0.05f);
+            }
+
+            if (DesktopDisplay != null)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawWireCube(DesktopDisplay.position, DesktopDisplay.lossyScale);
+            }
+        }
+    }
+}
